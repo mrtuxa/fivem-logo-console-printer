2c197c9aef888e618071da004c1bc808d08b2e05
tuxa <69870860+mrtuxa@users.noreply.github.com>
2022-05-05 17:16:32 +0200

    Update fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,6 @@
+fx_version 'cerculean'
+games { 'gta5' }
+author 'mrtuxa'
+description 'logo console printer at startup of your cfx server (only GTA5)'
+version '1.0.0'
+server_script 'src/server.lua'
